e2ef9a77567738c6ff358e84e981ea5d1bdcb4e8
georgia <georgia.z.leng@gmail.com>
2021-08-22 15:34:54 +1000

    removed githubactions env stuff / github actions outputs for image tagging, prefer to have this stuff in scripts so if need be I can run manually, so keeping those scripts/action the same in ci/cd seems gtm

--- bin/build-and-push-image.sh
+++ bin/build-and-push-image.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
+
 set -e
-# ecrname=$(aws cloudformation describe-stacks --stack-name basic-app-ecr --query "Stacks[0].Outputs[0].OutputValue" --output text)
 
-# commit=$(git describe --tags --always)
-# docker build -t "$ecrname:$commit" . 
-# docker push "$ecrname:$commit"
-# docker build -t "$ecrname:latest" . 
-# docker push "$ecrname:latest"
+ecrname=$(aws cloudformation describe-stacks --stack-name basic-app-ecr --query "Stacks[0].Outputs[0].OutputValue" --output text)
+commit=$(git describe --tags --always)
 
+docker build -t "$ecrname:$commit" . 
+docker push "$ecrname:$commit"
 
-docker build --build-arg AWS_ACCESS_KEY_ID="${{ secrets.AWS_ACCESS_KEY_ID }}" \
-    --build-arg AWS_SECRET_ACCESS_KEY="${{ secrets.AWS_SECRET_ACCESS_KEY }}" \
-    --build-arg AWS_DEFAULT_REGION="${{ secrets.AWS_DEFAULT_REGION }}" \
-    -t $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG .
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
-echo "::set-output name=image::$ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG"
+docker build -t "$ecrname:latest" . 
+docker push "$ecrname:latest"
+
